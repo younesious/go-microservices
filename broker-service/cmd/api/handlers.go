e81801b8c5eaa8c7f67f87c5fb356874b838bd95
younes Mahmoudi <younesious80@gmail.com>
2024-06-08 02:41:56 +0330

    add initial broker

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	_, _ = w.Write(out)
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,8 @@
+module github.com/younesious/go-microservices/broker-service
+
+go 1.21.10
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/cors v1.2.1
+)
